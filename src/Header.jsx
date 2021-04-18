bb2126ce39da67eeda14b98b34aa3933298e4513
AltafAlam Shaikh <gitforaltaf@gmail.com>
2021-04-18 20:26:24 +0530

    header & sidebar

--- src/Header.jsx
+++ src/Header.jsx
@@ -8,21 +8,31 @@ import NotificationsIcon from '@material-ui/icons/Notifications';
 import PersonIcon from '@material-ui/icons/Person';
 import logo from './youtube-logo.svg';
 
+function toggleSidebar() {
+    const content = document.querySelector('.app__content');
+    content.classList.toggle('active');
+}
+
+const submit = (e) => {
+    e.preventDefault();
+    alert('Search function coming soon:)');
+}
+
 function Header() {
     return (
         <nav className="header">
             <div className="header__left">
-                <button type="button" className="btn">
+                <button type="button" className="btn" onClick={toggleSidebar}>
                     <MenuIcon />
                 </button>
                 <img src={logo} alt="youtube" className="header__logo" />
             </div>
-            <div className="header__center">
+            <form className="header__center" onSubmit={submit} method="post">
                 <input type="text" placeholder="Search" class="form__input" />
-                <button type="button" class="btn header__search">
+                <button type="submit" class="btn header__search">
                     <SearchIcon className="header__search" />
                 </button>
-            </div>
+            </form>
             <div className="header__right">
                 <button type="button" className="btn"><VideoCallIcon /></button>
                 <button type="button" className="btn"><AppsIcon /></button>
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -4,6 +4,12 @@ import SidebarRow from './SidebarRow';
 import HomeIcon from '@material-ui/icons/Home';
 import WhatshotIcon from '@material-ui/icons/Whatshot';
 import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import HistoryIcon from '@material-ui/icons/History';
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 
 function Sidebar() {
     return (
@@ -11,6 +17,13 @@ function Sidebar() {
             <SidebarRow Icon={HomeIcon} name="Home" selected />
             <SidebarRow Icon={WhatshotIcon} name="Trending" />
             <SidebarRow Icon={SubscriptionsIcon} name="Subscriptions" />
+            <hr />
+            <SidebarRow Icon={VideoLibraryIcon} name="Library" />
+            <SidebarRow Icon={HistoryIcon} name="History" />
+            <SidebarRow Icon={PlayCircleFilledIcon} name="Your videos" />
+            <SidebarRow Icon={WatchLaterIcon} name="Watch later" />
+            <SidebarRow Icon={ThumbUpIcon} name="Liked videos" />
+            <SidebarRow Icon={KeyboardArrowDownIcon} name="Show more" />
         </div>
     )
 }
